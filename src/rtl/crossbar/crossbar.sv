c87ddba629487dd0bf0a3fa30a132c609f3234a3
Andrea Galimberti <andrea6.galimberti@mail.polimi.it>
2017-01-17 22:14:33 +0100

    [CROSSBAR] Changed data_o from reg to logic
    Changed the data type of data_o to better comply with the SystemVerilog standard

--- src/rtl/crossbar/crossbar.sv
+++ src/rtl/crossbar/crossbar.sv
@@ -1,7 +1,7 @@
 module crossbar #(parameter INPUT_NUM=2, parameter OUTPUT_NUM=2, parameter FLIT_SIZE=8)( 
   input [FLIT_SIZE-1:0] data_i [INPUT_NUM-1:0], 
   input [SEL_SIZE-1:0] sel_i [OUTPUT_NUM-1:0],
-  output reg [FLIT_SIZE-1:0] data_o [OUTPUT_NUM-1:0] 
+  output logic [FLIT_SIZE-1:0] data_o [OUTPUT_NUM-1:0] 
 ); 
 
   localparam [32:0] SEL_SIZE= utils::clogb2(INPUT_NUM); 
