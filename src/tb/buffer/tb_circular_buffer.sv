7b3341c9714a9863bb41f6a78c1a0485c1eed6bc
Andrea Galimberti <andrea6.galimberti@mail.polimi.it>
2017-03-01 17:46:34 +0100

    [TESTBENCH] Divided testbench in tasks
    Testbenches for both the crossbar and the circular buffer have been split in small and simple tasks, making the code easier to read, more easily maintainable and easier to read

--- src/tb/buffer/tb_circular_buffer.sv
+++ src/tb/buffer/tb_circular_buffer.sv
@@ -1,53 +1,79 @@
 `timescale 1ns / 1ps
 
 module tb_circular_buffer #(
-    parameter BUFFER_SIZE=8, 
+    parameter BUFFER_SIZE=8,
     parameter FLIT_SIZE=8
 );
-    
+
     integer i;
-    
+
     logic clk,rst;
     logic read_i;
     logic write_i;
-    
+
     logic [FLIT_SIZE-1:0] data_i;
     wire [FLIT_SIZE-1:0] data_o;
     wire is_full_o;
     wire is_empty_o;
-    
+
     initial
     begin
-    
+        dump_output();
+        initialize();
+        clear_reset();
+        write_until_saturate();
+        write_while_full();
+        read_and_write_while_full();
+        no_op();
+        read_until_empty();
+        read_while_empty();
+        no_op();
+        random_reads_writes();
+        #20 $finish;
+    end
+
+    always #5 clk = ~clk;
+
+    circular_buffer #(
+        .BUFFER_SIZE(BUFFER_SIZE),
+        .FLIT_SIZE(FLIT_SIZE)
+         )
+    circular_buffer (
+        .*
+    );
+
+    task dump_output();
         $dumpfile("out.vcd");
         $dumpvars(0, tb_circular_buffer);
-        
         // Dynamic dumping of the memory cells of the DUT
         for (i = 0; i < BUFFER_SIZE; i = i + 1)
             $dumpvars(0, tb_circular_buffer.circular_buffer.memory[i]);
-        
-        // Initialize input signals of the DUT
+    endtask
+
+    task initialize();
         clk     <= 0;
         rst     = 1;
         read_i  = 0;
         write_i = 0;
         data_i  = 0;
-        
-        // Clear rst
+    endtask
+
+    task clear_reset();
         repeat(5) @(posedge clk);
-            rst<=0;
-        
-        // writes up to buffer saturation
+            rst <= 0;
+    endtask
+
+    task write_until_saturate();
         repeat(12)
         begin
             @(posedge clk);
             read_i  <= 0;
             write_i <= is_full_o ? 0 : 1;
             data_i  <= {FLIT_SIZE{$random}};
-        
         end
-        
-        //writes try with full buffer
+    endtask
+
+    task write_while_full();
         repeat(2)
         begin
             @(posedge clk);
@@ -55,8 +81,9 @@ module tb_circular_buffer #(
             write_i <= 1;
             data_i  <= {FLIT_SIZE{$random}};
         end
-        
-        //simultaneous read/write with full buffer
+    endtask;
+
+    task read_and_write_while_full();
         repeat(2)
         begin
             @(posedge clk);
@@ -64,16 +91,18 @@ module tb_circular_buffer #(
             write_i <= 1;
             data_i  <= {FLIT_SIZE{$random}};
         end
-        
-        //no operations
+    endtask
+
+    task no_op();
         repeat(2)
         begin
             @(posedge clk);
             read_i  <= 0;
             write_i <= 0;
         end
-        
-        // reads until empty buffer
+    endtask
+
+    task read_until_empty();
         repeat(12)
         begin
             @(posedge clk);
@@ -81,8 +110,9 @@ module tb_circular_buffer #(
             write_i <= 0;
             data_i  <= {FLIT_SIZE{$random}};
         end
-        
-        //reads try with empty buffer
+    endtask
+
+    task read_while_empty();
         repeat(2)
         begin
             @(posedge clk);
@@ -90,16 +120,9 @@ module tb_circular_buffer #(
             write_i <= 0;
             data_i  <= {FLIT_SIZE{$random}};
         end
-        
-        //no operations
-        repeat(2)
-        begin
-            @(posedge clk);
-            read_i  <= 0;
-            write_i <= 0;
-        end
-        
-        //random, possibly simultaneous, reads/writes
+    endtask
+
+    task random_reads_writes();
         repeat(15)
         begin
             @(posedge clk);
@@ -107,20 +130,6 @@ module tb_circular_buffer #(
             write_i <= is_full_o ? 0 : $random;
             data_i  <= {FLIT_SIZE{$random}};
         end
-        
-        #20 $finish;
-    
-    end //end initial block
-    
-    // Clock update
-    always #5 clk =~clk;
-    
-    // DUT
-    circular_buffer #(
-        .BUFFER_SIZE(BUFFER_SIZE),
-        .FLIT_SIZE(FLIT_SIZE)
-         )
-    circular_buffer (
-        .* 
-    );
+    endtask
+
 endmodule
--- src/tb/crossbar/tb_crossbar.sv
+++ src/tb/crossbar/tb_crossbar.sv
@@ -3,21 +3,39 @@
 module tb_crossbar #(
     parameter INPUT_NUM = 4,
     parameter OUTPUT_NUM = 4,
-    parameter FLIT_SIZE = 4   
+    parameter FLIT_SIZE = 4
 );
-    
-    localparam [31:0] SEL_SIZE = $clog2(INPUT_NUM); 
+
+    localparam [31:0] SEL_SIZE = $clog2(INPUT_NUM);
 
     logic [FLIT_SIZE-1:0] data_i [INPUT_NUM-1:0];
     logic [SEL_SIZE-1:0] sel_i [OUTPUT_NUM-1:0];
     wire [FLIT_SIZE-1:0] data_o [OUTPUT_NUM-1:0];
-	
+
 	initial
 	begin
-	
-		$dumpfile("out.vcd");
+        dump_output();
+        random_test();
+		#5 $finish;
+	end
+
+	// DUT
+	crossbar #(
+        .INPUT_NUM(INPUT_NUM),
+        .OUTPUT_NUM(OUTPUT_NUM),
+        .FLIT_SIZE(FLIT_SIZE)
+        )
+    crossbar (
+        .*
+    );
+
+    //tasks
+    task dump_output();
+        $dumpfile("out.vcd");
 		$dumpvars(0, tb_crossbar);
-	
+    endtask
+
+    task random_test();
 	    repeat(20)
 	    begin
 		#5
@@ -26,20 +44,6 @@ module tb_crossbar #(
         for(int i=0;i<OUTPUT_NUM;i++)
             sel_i[i] = {SEL_SIZE{$random}};
         end
-		
-		
-		#5 $finish;
-	
-	end
-	
-	// DUT	
-	crossbar #(
-        .INPUT_NUM(INPUT_NUM),
-        .OUTPUT_NUM(OUTPUT_NUM),
-        .FLIT_SIZE(FLIT_SIZE)
-        )
-    crossbar ( 
-        .*
-    ); 
+    endtask
 
 endmodule
