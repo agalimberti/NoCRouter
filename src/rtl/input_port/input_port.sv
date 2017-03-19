caa3e1d52b1c24f794117b16960ccd97b77359e3
Filippo Testa <filippo2.testa@mail.polimi.it>
2017-03-19 12:19:41 -0400

    [INPUTPORT] Added support for new interfaces
    The IP module contains a RC unit and it has two different interfaces for the crossbar and the swicth allocator.

--- src/rtl/input_port/input_port.sv
+++ src/rtl/input_port/input_port.sv
@@ -1,13 +1,23 @@
 import noc_params::*;
 
 module input_port #(
-    parameter BUFFER_SIZE = 8
-    )(input clk, rst, rc_unit2input_port rc, flit_t data_i);
+        parameter BUFFER_SIZE = 8
+    )(  input clk, input rst, 
+        input_port2crossbar if_crossbar, 
+        input_port2switch_allocator if_sa
+    );
 
     input_buffer
         #(.BUFFER_SIZE(BUFFER_SIZE)) 
         ib0(
             .*
+        );
+    
+    rc_unit
+        #(.X_CURRENT(0),
+          .Y_CURRENT(0))
+        rc(
+            // TODO
         );
         
     always_ff @(posedge clk)  
