e6b28e3d39d8229d5aa3bdb8ea79f43c54d46919
Filippo Testa <filippo2.testa@mail.polimi.it>
2017-03-19 12:10:27 -0400

    [ROUTER] Moved out input_port2crossbar interface
    The input_port2crossbar interface has been moved out of the router module and implemented as a separated sv file.

--- src/rtl/router.sv
+++ src/rtl/router.sv
@@ -2,23 +2,7 @@ import noc_params::*;
 
 module router(input clk, input rst, router2router if_router);
 
-
-
-interface input_port2crossbar;
-    
-    flit_t flit;
-    
-    modport ip(
-        output flit
-    );
-    
-    modport xbar(
-        input flit
-    );
-endinterface
-
-
-
+    input_port2crossbar ip0_2_xbar;
 
     input_port
         #(.BUFFER_SIZE(8))
@@ -26,9 +10,7 @@ endinterface
             .clk(clk),
             .rst(rst),
         );
+     
     crossbar xbar;
-    input_port2crossbar ip0_2_xbar;
     
-    
-
 endmodule
