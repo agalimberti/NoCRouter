a91a9b07cc4a27920c12f8d4d555e27be1085779
Filippo Testa <filippo2.testa@mail.polimi.it>
2017-03-20 14:28:44 -0400

    [ROUTER] Renamed router interface
    The interface that connects the router with another one has been renamed accordingly to the adopted convention. Moreover, the binding with the internal Input Port has been simplified exploiting the .*

--- src/rtl/router.sv
+++ src/rtl/router.sv
@@ -1,17 +1,18 @@
 import noc_params::*;
 
-module router(input clk, input rst, router2router if_router);
+module router(
+    input clk, 
+    input rst, 
+    router2router router_if
+);
 
     input_port2crossbar ip0_2_xbar;
-
+    
     input_port
         #(.BUFFER_SIZE(8))
         ip0(
-            .clk(clk),
-            .rst(rst),
+            .*,
             .crossbar_if(ip0_2_xbar)
         );
-     
-    crossbar xbar;
     
 endmodule
