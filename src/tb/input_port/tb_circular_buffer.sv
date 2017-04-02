5da00dc03fe4a78e4e1b02493767ea72e24af8a4
Alberto Zeni <alberto.zeni@mail.polimi.it>
2017-04-02 11:25:23 +0200

    [BUFFER] Fixed queue and added number of operations
    The queue has been fixed for random writes and reads, also the flit that are generated now depend on the number of operation that have been done in order to easily check the in/out flits.

--- src/tb/input_port/tb_circular_buffer.sv
+++ src/tb/input_port/tb_circular_buffer.sv
@@ -6,8 +6,8 @@ module tb_circular_buffer #(
     parameter BUFFER_SIZE=8
 );
 
-    integer i;
-    integer random_test = 8;
+    int i;
+    int num_operation;
     
     logic clk,rst;
     logic read_i;
@@ -27,10 +27,6 @@ module tb_circular_buffer #(
         dump_output();
         initialize();
         clear_reset();
-        repeat(random_test)
-            write();
-        repeat(random_test)
-            read();
         #20 $finish;
     end
 
@@ -56,6 +52,7 @@ module tb_circular_buffer #(
         
         for (i = 0; i < BUFFER_SIZE; i = i + 1)
             $dumpvars(0, tb_circular_buffer.circular_buffer.memory[i]);
+        i = 0;
     endtask
 
     task initialize();
@@ -63,6 +60,7 @@ module tb_circular_buffer #(
         rst     = 1;
         read_i  = 0;
         write_i = 0;
+        num_operation = 0;
     endtask
 
     task clear_reset();
@@ -73,48 +71,67 @@ module tb_circular_buffer #(
     //the read task first pops out a flit at the front of the flit_queue
     //then it reads a flit from the buffer and compares the two
     task read();
-        flit_read = flit_queue.pop_front;
-        @(posedge clk);
-        write_i <= 0;
-        read_i <= is_empty_o ? 0 : 1;
-        if(~check_flits & i!=8)
-            $display("[READ] FAILED");
-        else $display("[READ] PASSED");
-        i = 0;
+    	if(i == 0)
+    		return;
+    	else
+    	begin
+    		flit_read=flit_queue.pop_front();
+    		@(posedge clk);
+    		write_i <= 0;
+    	    read_i <= 1;
+    	    data_i <= flit_x;
+    	    i = i - 1;
+    	    num_operation = num_operation + 1;
+    	    @(posedge clk);
+    	    write_i <= 0;
+    	   	read_i <= 0;
+    	    data_i <= flit_x;
+       	 	if(~check_flits)
+            	$display("[READ] FAILED");
+        	else $display("[READ] PASSED");
+        end
     endtask
     
 
     //the write task first inserts a flit in the flit_queue
     //then the same flit is written into the buffer
     task write();
-        insert_in_queue();
-        @(posedge clk);
-        write_i <= is_full_o ? 0 : 1;
-        read_i <=0;
-        data_i <= flit_written;
-        if(is_empty_o & flit_queue.size() > 2)
-            $display("[WRITE] FAILED");
-        else $display("[WRITE] PASSED");
+    	if(i == BUFFER_SIZE - 1)
+    		return;
+    	else
+    	begin
+    		insert_in_queue();
+        	@(posedge clk);
+        	write_i <= 1;
+        	read_i <= 0;
+        	data_i <= flit_written;
+        	flit_queue.push_back(flit_written);
+        	if(is_empty_o & flit_queue.size() > 2)
+        		$display("[WRITE] FAILED");
+        	else $display("[WRITE] PASSED");
+        	i = i + 1;
+        	num_operation = num_operation + 1;
+        end
     endtask
+    
 
     task insert_in_queue();
-         flit_written.flit_label <= HEAD;
-        flit_written.vc_id <= {VC_SIZE{flit_queue.size()}};
-        flit_written.data.head_data.x_dest <= {DEST_ADDR_SIZE_X{flit_queue.size()}};
-        flit_written.data.head_data.y_dest <= {DEST_ADDR_SIZE_Y{flit_queue.size()}}; 
-          flit_written.data.head_data.head_pl <= {HEAD_PAYLOAD_SIZE{flit_queue.size()}};
-          flit_queue.push_back(flit_written);
+ 		flit_written.flit_label <= HEAD;
+        flit_written.vc_id <= {VC_SIZE{num_operation}};
+        flit_written.data.head_data.x_dest <= {DEST_ADDR_SIZE_X{num_operation}};
+    	flit_written.data.head_data.y_dest <= {DEST_ADDR_SIZE_Y{num_operation}}; 
+  	    flit_written.data.head_data.head_pl <= {HEAD_PAYLOAD_SIZE{num_operation}};
     endtask
 
     function logic check_flits();
-        if(flit_read.flit_label == data_o.flit_label &
-           flit_read.vc_id == data_o.vc_id &
-           flit_read.data.head_data.x_dest == data_o.data.head_data.x_dest &
-           flit_read.data.head_data.y_dest == data_o.data.head_data.y_dest &
-           flit_read.data.head_data.head_pl == data_o.data.head_data.head_pl)
-            check_flits = 1;
-        else
-            check_flits = 0;   
+    	if(flit_read.flit_label == data_o.flit_label &
+    	   flit_read.vc_id == data_o.vc_id &
+    	   flit_read.data.head_data.x_dest == data_o.data.head_data.x_dest &
+    	   flit_read.data.head_data.y_dest == data_o.data.head_data.y_dest &
+    	   flit_read.data.head_data.head_pl == data_o.data.head_data.head_pl)
+    	    check_flits = 1;
+    	else
+    		check_flits = 0;   
     endfunction 
     
 endmodule
