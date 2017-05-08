50b8c1d342e650b7468f8c50d90c07160236751d
Andrea Galimberti <andrea6.galimberti@mail.polimi.it>
2017-05-08 23:28:51 +0200

    [IB2SA] Added signals to the Input Block - Switch Allocator interface
    The signals for the request of Switch Allocation from each Input Buffer and to inform the Allocator of the assigned VC at the downstream Router have been added to the interface.

--- src/rtl/if/input_block2switch_allocator.sv
+++ src/rtl/if/input_block2switch_allocator.sv
@@ -2,20 +2,26 @@ import noc_params::*;
 
 interface input_block2switch_allocator;
 
-    port_t [VC_NUM-1 : 0] out_port [PORT_NUM-1:0];
-    logic [VC_SIZE-1 : 0] vc_sel [PORT_NUM-1:0];
+    port_t [VC_NUM-1:0] out_port [PORT_NUM-1:0];
+    logic [VC_SIZE-1:0] vc_sel [PORT_NUM-1:0];
     logic valid_sel [PORT_NUM-1:0];
+    logic [VC_SIZE-1:0] downstream_vc [PORT_NUM-1:0][VC_NUM-1:0];
+    logic switch_request [PORT_NUM-1:0][VC_NUM-1:0];  //TODO: from Input Buffer, asserted when in SA state
 
     modport input_block (
-        output out_port,
         input vc_sel,
-        input valid_sel
+        input valid_sel,
+        output out_port,
+        output downstream_vc,
+        output switch_request
     );
 
     modport switch_allocator (
-        input out_port,
         output vc_sel,
-        output valid_sel
+        output valid_sel,
+        input out_port,
+        input downstream_vc,
+        input switch_request
     );
 
 endinterface
