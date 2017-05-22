9f78db47fc71df80b1e0eebc023a2d87592cd0bc
Andrea Galimberti <andrea6.galimberti@mail.polimi.it>
2017-05-22 10:56:45 +0200

    [BUFFER+PORT] Modified C/I Buffer and Input Port modules with no-VC flit
    The Circular Buffer, Input Buffer and Input Port have been modified to support the storage in memory of flits stripped of their current Virtual Channel Identifier, as defined in the previous commit.

--- src/rtl/input_port/input_buffer.sv
+++ src/rtl/input_port/input_buffer.sv
@@ -4,7 +4,7 @@ module input_buffer #(
     parameter BUFFER_SIZE = 8,
     parameter PIPELINE_DEPTH = 5
 )(
-    input flit_t data_i,
+    input flit_novc_t data_i,
     input read_i,
     input write_i,
     input [VC_SIZE-1:0] vc_new_i,
@@ -33,7 +33,7 @@ module input_buffer #(
     logic vc_allocatable_next;
     logic error_next;
 
-    flit_t read_flit;
+    flit_novc_t read_flit;
 
     port_t out_port_next;
 
